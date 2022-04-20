890bcb1580db3bf9307cd6e6d8f24f83a0b8891e
Dil Ahdan <dilrajahdan@gmail.com>
2022-04-20 02:05:10 +0100

    sexed up nav

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -9,6 +9,15 @@
       temporary
     >
       <v-list>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ title }}
+              <b>{{ title2 }}</b>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider></v-divider>
         <v-list-item
           v-for="(item, i) in items"
           :key="i"
@@ -25,7 +34,7 @@
     </v-navigation-drawer>
     <v-app-bar dark clipped-left fixed app>
       <v-container>
-        <v-row>
+        <v-row align="center" justify="center">
           <v-app-bar-nav-icon
             v-show="$vuetify.breakpoint.xs && !noLinks"
             @click.stop="drawer = !drawer"
@@ -39,22 +48,25 @@
             </nuxt-link>
           </v-toolbar-title>
           <v-spacer />
-          <div v-show="!noLinks">
-            <v-tabs
+          <!-- <div > -->
+          <!-- <v-tabs
               v-show="$vuetify.breakpoint.smAndUp"
               class="mr-3"
               background-color="transparent"
-            >
-              <v-tab
-                v-for="item in items"
-                :key="item.title"
-                :disabled="item.disabled"
-                :to="item.to"
-                :class="item.outlined ? 'd-flex' : 'd-none d-sm-flex'"
-                >{{ item.title }}</v-tab
-              >
-            </v-tabs>
-          </div>
+            > -->
+          <!-- {{ $vuetify.breakpoint.name }} -->
+          <v-btn
+            v-for="item in items"
+            v-show="!noLinks || $vuetify.breakpoint.mdAndUp"
+            :key="item.title"
+            :to="item.to"
+            class="ml-2"
+            :color="item.outlined ? 'red' : ''"
+            :class="item.outlined ? 'd-flex' : 'd-none d-md-flex'"
+            >{{ item.title }}</v-btn
+          >
+          <!-- </v-tabs> -->
+          <!-- </div> -->
         </v-row></v-container
       >
     </v-app-bar>
@@ -75,6 +87,11 @@ export default {
       clipped: false,
       drawer: false,
       items: [
+        {
+          title: 'Home',
+          to: '/',
+        },
+
         {
           title: 'Lab directory',
           to: '/cannabis-testing-labs',
