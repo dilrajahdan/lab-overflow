36eabbff7be5aec0414eecc30bdf0595af30008f
Dil Ahdan <dilrajahdan@gmail.com>
2022-06-08 23:47:23 +0100

    add calendly to thank you page

--- pages/thank-you.vue
+++ pages/thank-you.vue
@@ -13,7 +13,9 @@
               <h1 class="overline">Thank you</h1>
             </v-card-title>
             <v-card-subtitle class="">
-              <h2 class="display-1">Speak to you soon</h2>
+              <h2 class="display-1">
+                Book a call to see how we can help each other out
+              </h2>
             </v-card-subtitle>
           </v-col>
         </v-row>
@@ -21,9 +23,21 @@
     </v-card>
     <v-container>
       <v-card>
-        <v-card-actions>
+        <!-- Calendly inline widget begin -->
+        <div
+          class="calendly-inline-widget"
+          data-url="https://calendly.com/dil-ahdan/lab-chat?hide_event_type_details=1&hide_gdpr_banner=1"
+          style="min-width: 320px; height: 630px"
+        ></div>
+        <script
+          type="text/javascript"
+          src="https://assets.calendly.com/assets/external/widget.js"
+          async
+        ></script>
+        <!-- Calendly inline widget end -->
+        <!-- <v-card-actions>
           <v-btn to="/">Back to home</v-btn>
-        </v-card-actions>
+        </v-card-actions> -->
       </v-card>
     </v-container>
   </section>
