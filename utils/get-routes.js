62e7567842f838b231cee727ecb70d777c479583
Dil Ahdan <dilrajahdan@gmail.com>
2022-07-12 13:04:35 +0100

    comment out jobs and lab directory - todo

--- utils/get-routes.js
+++ utils/get-routes.js
@@ -1,11 +1,35 @@
 export default async () => {
   const { $content } = require('@nuxt/content')
-  const files = await $content('blog', { deep: true }).only(['path']).fetch()
 
-  const routes = files.map((file) => (file.path === '/index' ? '/' : file.path))
+  const blog = await $content('blog', { deep: true }).only(['path']).fetch()
+  const blogRoutes = blog.map((file) =>
+    file.path === '/index' ? '/' : file.path
+  )
 
-  console.log('routes', routes)
-  return routes
+  // TODO: Maps jobs to a location.
+  // Once you know this you can generate the correct
+  // dynamic routes
+
+  // const jobs = await $content('jobs', { deep: true })
+  //   .only(['path', 'slug'])
+  //   .fetch()
+  // const jobRoutes = jobs.map((file) => {
+  //   console.log('file', file)
+  //   return file.path === '/index' ? '/' : file.path
+  // })
+
+  // const labs = await $content('labs', { deep: true })
+  //   .only(['path', 'slug'])
+  //   .fetch()
+  // const labRoutes = labs.map((file) =>
+  //   file.path === '/index' ? '/' : file.path
+  // )
+
+  // console.log('blogRoutes', blogRoutes)
+  // console.log('jobRoutes', jobRoutes)
+  console.log('labRoutes', labRoutes)
+  // return [...blogRoutes, ...jobRoutes, ...labRoutes]
+  return blogRoutes
 
   // return files.map((file) => (file.path === '/index' ? '/' : file.path))
 }
